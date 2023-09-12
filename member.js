baf914c6aa950f8a95a9423ee4d4d11f33a16639
Juan Carlos Mellizo <juancho.coroto@gmail.com>
2023-09-12 16:10:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberInfo = document.getElementById("memberInfo");
+    var skillsInfo = document.getElementById("skillsInfo");
+    var projectsInfo = document.getElementById("projectsInfo");
+    var contactInfo = document.getElementById("contactInfo");
+    var aboutInfo = document.getElementById("aboutInfo");
+    member.style.backgroundColor = "#f5f5f5";
+    skills.style.backgroundColor = "#e7e7e7";
+    projects.style.backgroundColor = "#f5f5f5";
+    contact.style.backgroundColor = "#f5f5f5";
+    about.style.backgroundColor = "#f5f5f5";
+    memberInfo.style.display = "block";
+    skillsInfo.style.display = "none";
+    projectsInfo.style.display = "none";
+    contactInfo.style.display = "none";
+    aboutInfo.style.display = "none";
+}
